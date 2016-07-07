effc4d13abdb6ab4018e1f6b449052621dad6e1e
Nathaniel Grevatt <nqg3ux@virginia.edu>
2016-07-07 16:45:40 -0400

    implementing set optimization

--- uninit_filter.py
+++ uninit_filter.py
@@ -17,11 +17,12 @@ def find_uninit_classes(input_file):
 
     for file_name in data.measured_files():
         with open(file_name, 'r') as f:
+            covered_lines = set(data.lines(file_name))
             for i, line in enumerate(f):
                 current_line = i + 1
                 if line.startswith('class'):
                     current_cls = re_cls.match(line).group(1)
-                elif re_init.search(line) and (current_line) not in data.lines(file_name):
+                elif re_init.search(line) and current_line not in covered_lines:
                     if file_name not in uninit_dict:
                         uninit_dict[file_name] = []
                     uninit_dict[file_name].append(current_cls)
@@ -31,8 +32,10 @@ def find_uninit_classes(input_file):
 
 def output(uninit_dict):
     for file_name, cls_list in uninit_dict.items():
+        print(file_name + ":")
         for cls_name in cls_list:
-            print('{}: {}'.format(file_name, cls_name))
+            print('\t{}'.format(cls_name))
+        print("\n")
 
 
 def main():
