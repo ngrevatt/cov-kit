cadfff248060c8cc3858b18bddf2ad736224e2f3
Nathaniel Grevatt <nqg3ux@virginia.edu>
2016-07-07 12:48:55 -0400

    find_uninit_classes as a function which creates a dictionary of uninitialized classes, printing in occurs in main

--- uninit_filter.py
+++ uninit_filter.py
@@ -3,25 +3,33 @@ import re
 from coverage import CoverageData
 
 
-def main():
+def find_uninit_classes(coverage_file):
     data = CoverageData()
     data.read_file('.coverage')
 
     re_cls = re.compile(r'^class\s(.+)[\(|:]')
     re_init = re.compile(r'def\s__init__')
     current_cls = None
+    uninit_dict = {}
 
-    with open('uninitialized_classes.txt', 'w') as out:
-        for file in data.measured_files():
-            with open(file, 'r') as f:
-                for i, line in enumerate(f):
-                    if line.startswith('class'):
-                        try:
-                            current_cls = re_cls.match(line).group(1)
-                        except AttributeError:
-                            import pdb; pdb.set_trace()
-                    elif re_init.search(line) and (i + 1) not in data.lines(file):
-                        out.write('%s %s\n' % (file, current_cls))
+    for file in data.measured_files():
+        with open(file, 'r') as f:
+            for i, line in enumerate(f):
+                current_line = i + 1
+                if line.startswith('class'):
+                    current_cls = re_cls.match(line).group(1)
+                elif re_init.search(line) and (current_line) not in data.lines(file):
+                    if file not in uninit_dict:
+                        uninit_dict[file] = []
+                    uninit_dict[file].append(current_cls)
+
+    return uninit_dict
+
+
+def main():
+    uninit = find_uninit('.coverage')
+    for file in uninit:
+        print(file, uninit[file], "\n")
 
 
 if __name__ == '__main__':
