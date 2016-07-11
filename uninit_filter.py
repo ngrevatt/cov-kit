7cf88879ebefb273ffb9278bfb9c9aff8b583259
Nathaniel Grevatt <nqg3ux@virginia.edu>
2016-07-11 14:12:51 -0400

    added sorting of files by most lines covered, get_cov_dat helper function, corrected error with identifying classes(?), added default for input_file command line arg

--- uninit_filter.py
+++ uninit_filter.py
@@ -4,11 +4,18 @@ import argparse
 from coverage import CoverageData
 
 
-def find_uninit_classes(input_file):
+def get_cov_data(input_file):
     if input_file is None:
         input_file = '.coverage'
     data = CoverageData()
     data.read_file(input_file)
+    return data
+
+
+def find_uninit_classes(input_file):
+    if input_file is None:
+        input_file = '.coverage'
+    data = get_cov_data(input_file)
 
     re_cls = re.compile(r'^class\s(.+)[\(|:]')
     re_init = re.compile(r'def\s__init__')
@@ -19,7 +26,7 @@ def find_uninit_classes(input_file):
         with open(file_name, 'r') as f:
             covered_lines = set(data.lines(file_name))
             for i, line in enumerate(f):
-                current_line = i + 1
+                current_line = i
                 if line.startswith('class'):
                     current_cls = re_cls.match(line).group(1)
                 elif re_init.search(line) and current_line not in covered_lines:
@@ -30,10 +37,13 @@ def find_uninit_classes(input_file):
     return uninit_dict
 
 
-def output(uninit_dict):
-    for file_name, cls_list in uninit_dict.items():
+def output(input_file, uninit_dict):
+    data = get_cov_data(input_file)
+    output_map = data.line_counts(fullpath=True)
+    s = sorted(uninit_dict.keys(), reverse=True, key=lambda x: output_map[x])
+    for file_name in s:
         print(file_name + ":")
-        for cls_name in cls_list:
+        for cls_name in uninit_dict[file_name]:
             print('\t{}'.format(cls_name))
         print("\n")
 
@@ -44,7 +54,7 @@ def main():
     args = parser.parse_args()
 
     uninit = find_uninit_classes(args.input)  # create dictionary of files and uninitialized classes
-    output(uninit)
+    output(args.input, uninit)
 
 
 if __name__ == '__main__':
