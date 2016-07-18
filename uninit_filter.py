33f3760c9c2a2ba535c28cbf64cf78d2bb4f3375
Nathaniel Grevatt <nqg3ux@virginia.edu>
2016-07-18 15:26:39 -0400

    added searching for uncovered functions and methods, will only be output if not part of an uninitiazlied class

--- uninit_filter.py
+++ uninit_filter.py
@@ -24,23 +24,17 @@ def get_analysis(cov, data):
     return analysis
 
 
-def find_uncovered(analysis):
+def find_uninitialized(analysis):
     re_cls = re.compile(r'^class\s(.+)[\(|:]')
     re_init = re.compile(r'def\s__init__')
-    re_func = re.compile(r'^def\s(.+)[\(|:]')
     current_cls = None
-    current_func = None
+    in_init = False
     coverages = {}
     classes = {}
-    functions = {}
-
     for file_name in analysis:  # keys in analysis are file_names
         with open(file_name, 'r') as f:
             _, executable_lines, excluded_lines, missing_lines, __ = analysis[file_name]
             uncov_classes = []
-            uncov_functions = []
-            in_init = False
-            in_func = False
             for line_number, line in enumerate(f, 1):
                 # handling uninitialized classes
                 if line.startswith('class'):
@@ -51,7 +45,32 @@ def find_uncovered(analysis):
                     if line_number in missing_lines:
                         uncov_classes.append(current_cls)
                     in_init = False
-                # handling uninitalized functions
+            if executable_lines:
+                coverages[file_name] = (len(executable_lines) - len(missing_lines)) / len(executable_lines)
+            if uncov_classes:
+                classes[file_name] = uncov_classes 
+    return coverages, classes
+
+
+def find_uncovered(analysis, uninitialized_classes):
+    re_cls = re.compile(r'^class\s(.+)[\(|:]')
+    re_func = re.compile(r'^def\s(.+)[\(|:]')
+    re_meth = re.compile(r'\s{1,}def\s(.+)[\(|:]')
+    current_func = None
+    current_meth = None
+    in_func = False
+    in_meth = False
+    functions = {}
+    methods = {}
+    for file_name in analysis:  # keys in analysis are file_names
+        with open(file_name, 'r') as f:
+            _, executable_lines, excluded_lines, missing_lines, __ = analysis[file_name]
+            uncov_functions = []
+            uncov_methods = []
+            for line_number, line in enumerate(f, 1):
+                if line.startswith('class'):
+                    current_cls = re_cls.match(line).group(1)
+                # handling unused functions
                 if line.startswith('def'):
                     current_func = re_func.match(line).group(1)
                     in_func = True
@@ -59,24 +78,34 @@ def find_uncovered(analysis):
                     if line_number in missing_lines:
                         uncov_functions.append(current_func)
                     in_func = False
-            if executable_lines:
-                coverages[file_name] = (len(executable_lines) - len(missing_lines)) / len(executable_lines)
-            if uncov_classes:
-                classes[file_name] = uncov_classes 
+                # handling unused methods
+                if re_meth.match(line):
+                    current_meth = re_meth.match(line).group(1)
+                    in_meth = True
+                elif in_meth and line_number in executable_lines:
+                    if line_number in missing_lines:
+                        if file_name in uninitialized_classes:
+                            if current_cls not in uninitialized_classes[file_name]:
+                                uncov_methods.append(current_cls + '.' + current_meth)
+                        else:
+                            uncov_methods.append(current_cls + '.' + current_meth)
+                    in_meth = False
             if uncov_functions:
                 functions[file_name] = uncov_functions 
-    return coverages, classes, functions
-
-
+            if uncov_methods:
+                methods[file_name] = uncov_methods
+    return functions, methods
+   
+    
 def trim_file_name(data, file_name):
     directory_path = os.path.dirname(os.path.commonpath(data.measured_files()))
     return file_name.split(directory_path)[-1]
 
 
-def output(data, coverages, classes, functions):
+def output(data, coverages, classes, functions, methods):
     s = sorted(coverages, reverse=True, key=coverages.get)
     for file_name in s:
-        if file_name in classes or file_name in functions:
+        if file_name in classes or file_name in functions or file_name in methods:
             trimmed_name = trim_file_name(data, file_name)
             # percent_cmverage is the first item in the list
             print('{} ({:2.2f}%):'.format(trimmed_name, coverages[file_name]*100))
@@ -88,6 +117,10 @@ def output(data, coverages, classes, functions):
                 print('Unused Functions:')
                 for func_name in functions[file_name]:
                     print('\t{}'.format(func_name))
+            if file_name in methods:
+                print('Unused Methods:')
+                for meth_name in methods[file_name]:
+                    print('\t{}'.format(meth_name))
             print('\n')
 
 
@@ -100,9 +133,10 @@ def main():
     data = get_cov_data(args.input)
     analysis = get_analysis(cov, data)
 
-    # create dictionary of files and uninitialized classes
-    coverages, classes, functions = find_uncovered(analysis)
-    output(data, coverages, classes, functions)
+    # get and outputs coverages, uninitialized classes, and uncovered functions and methods
+    coverages, classes = find_uninitialized(analysis)
+    functions, methods = find_uncovered(analysis, classes)
+    output(data, coverages, classes, functions, methods)
 
 
 if __name__ == '__main__':
